c60ba6a4ba72cc378dbfe7aedd0e2b6bdf2b0a3d
Ganesh Jadhav <36586236+ganesh20215@users.noreply.github.com>
2025-05-08 08:43:57 +0530

    Update BasicOfTestNG.java

--- src/test/java/testng/basicoftestng/BasicOfTestNG.java
+++ src/test/java/testng/basicoftestng/BasicOfTestNG.java
@@ -19,7 +19,6 @@ public class BasicOfTestNG {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.amazon.in");
         driver.manage().window().maximize();
-        Thread.sleep(3000);
         driver.close();
     }
 
